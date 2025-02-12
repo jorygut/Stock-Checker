ee308ed014bd9d31ed278993e7f058ed4cb6e201
jorygut <118095034+jorygut@users.noreply.github.com>
2025-02-12 12:01:20 -0500

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -19,7 +19,7 @@ import yfinance as yf
 
 # Extract article info from news API
 def fetch_articles(name):
-    API_KEY = "7c41beb12c4e426488da09b8200c61d5"
+    API_KEY = "INSERT HERE"
     BASE_URL = "https://newsapi.org/v2/everything"
 
     params = {
